2877c6c49da4339c86517a136d94ea6c54fa6225
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-01-06 09:29:32 -0400

    Introduction to FormBuilder Validators.required More FormControls Nested FormGroups

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {states} from '../../data-model'
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent {
+  heroForm: FormGroup; // <--- heroForm is of type FormGroup
+  states = states;
 
-  heroForm = new FormGroup({
-    name: new FormControl()
-  })
-  
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { // <--- inject FormBuilder
+    this.createForm();
   }
 
+  createForm() {
+    this.heroForm = this.fb.group({ // <-- the parent FormGroup
+      name: ['', Validators.required ], // <--- the FormControl called "name"
+      address: this.fb.group({ // <-- the child FormGroup
+        street: '',
+        city: '',
+        state: '',
+        zip: ''
+      }),
+      power: '',
+      sidekick: ''
+    });
+  }
 }
